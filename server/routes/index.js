0e1d6b4fbbe76af908dfe35ad9bc1ead35567c98
Christopher Pachomski <cpachomski@gmail.com>
2016-09-12 18:22:12 -0400

    create navbar, and begin styling

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,11 @@ import express from 'express'
 
 const router = express.Router()
 
+
+router.get('/', (req, res, next) => {
+	return res.render('index', { title: 'index' })
+})
+
 router.get('/login', (req, res, next) => {
 	return res.render('login', { title: 'login' })
 })
