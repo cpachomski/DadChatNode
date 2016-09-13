b2503915e05456955c60d0dd275d8b09f94439b8
Christopher Pachomski <cpachomski@gmail.com>
2016-09-13 16:21:31 -0400

    create logout route

---
+++ server/routes/logout.js
@@ -0,0 +1,16 @@
+import express from 'express'
+const router = express.Router()
+
+router.get('/logout', (req, res, next) => {
+	if (req.session) {
+		req.session.destroy((err) => {
+			if (err) {
+				return next(err)
+			} else {
+				return res.redirect('/')
+			}
+		})
+	}
+})
+
+export default router
