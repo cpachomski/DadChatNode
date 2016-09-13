ba133d71ad8dec1afa276b7f5f75f7bc2a4a8e72
Christopher Pachomski <cpachomski@gmail.com>
2016-09-13 16:21:06 -0400

    finish login routes

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import User from '../models/user'
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
@@ -6,7 +7,25 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
-	res.send('AHHHH')
+	const { email, password } = req.body
+
+	if (email && password) {
+		User.authenticate(email, password, (err, user) => {
+			if (err || !user) {
+				let err = new Error('Wrong email or password cmon jeez')
+				err.status= 401
+				return next(err)
+			} else {
+				//create user session
+				req.session.userId = user._id
+				return res.resdirect('/rooms')
+			}
+		})
+	} else {
+		let err = new Error('Gotta give me both the email and the password')
+		err.status = 401
+		return next(err)
+	}
 })
 
 export default router;
