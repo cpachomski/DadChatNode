c5c6d11835a7c98bf9ca0d0f8c8d086cdc33ac95
Christopher Pachomski <cpachomski@gmail.com>
2016-09-13 16:21:14 -0400

    setup register routes

--- server/routes/register.js
+++ server/routes/register.js
@@ -7,7 +7,41 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
-	res.send('yo')
+	const { firstName,
+			lastName,
+			email,
+			password,
+			passwordConfirmation } = req.body
+
+	if (firstName &&
+		lastName  &&
+		email     &&
+		password  &&
+		passwordConfirmation) {
+
+		if (password !== passwordConfirmation) {
+			let err = new Error('Passwords gotta be the same')
+			err.status = 400
+			next(err)
+		}
+
+		let userData = {
+			firstName: firstName,
+			lastName: lastName,
+			email: email,
+			password: password
+		}
+
+		User.create(userData, (err, user) => {
+			if (err) {
+				next(err)
+			} else {
+				//create user session
+				req.session.userId = user._id
+				return res.redirect('/rooms')
+			}
+		})
+	}
 })
 
 export default router
