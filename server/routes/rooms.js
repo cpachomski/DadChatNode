6c454971f3d4e5c7b54936d9dfeae0dbbcfe3483
Christopher Pachomski <cpachomski@gmail.com>
2016-09-21 16:41:46 -0400

    update rooms routes to create new rooms and rerender

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,14 +1,43 @@
 import express from 'express'
+import Room from '../models/room'
+import User from '../models/user'
 import { requiresLoggedIn } from '../middleware'
 
 const router = express.Router()
 
 router.get('/', requiresLoggedIn, (req, res, next) => {
-	res.render('rooms', {title: 'rooms' })
+	const { userId } = req.session
+	let rooms = []
+
+	User.findOne({ _id: userId }, (err, user) => {
+		user.rooms.map((room) => {
+			rooms.push(room)
+		})
+	}).then(() => {
+		res.render('rooms', {title: 'rooms', rooms: rooms})
+	})
+
 })
 
 router.post('/', (req, res, next) => {
-	res.send("uhhhhhh it's not quite there yet")
+	const { name } = req.body
+	const userId = req.session.userId
+
+	if (name && userId) {
+		Room.create({ name: name, admin: userId }, (err, room, done) => {
+			User.findOne({ _id: userId} , (err, user) => {
+				user.rooms.push({_id: room._id, name: room.name })
+				user.save((err) => {
+					if (err) {
+						next(err)
+					}
+					res.redirect(req.get('Referer'))
+				})
+
+			})
+		})
+	}
+
 })
 
 export default router
