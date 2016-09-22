6832872199690b3b41884e4378d593ba7eb591ed
Christopher Pachomski <cpachomski@gmail.com>
2016-09-22 14:30:15 -0400

    remove message model. rather deal with the message model myself than to perform multiple queries

--- server/models/message.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const MessageSchema = new mongoose.Schema({
-	user: [{ type: number, ref: 'User', required: true }],
-	content: {
-		type: String,
-		required: true
-	}
-})
