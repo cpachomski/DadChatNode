8870bc584d73555a4e6340599b4e3cfdf946eb35
Christopher Pachomski <cpachomski@gmail.com>
2016-09-22 16:54:21 -0400

    add search route to return results to client

---
+++ server/routes/search.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import User from '../models/user'
+const router = express.Router()
+
+router.post('/users', (req, res, next) => {
+	const { query } = req.body
+
+	User.find({$text: {$search: query}}, {score: {$meta: "textScore"}}).sort({score:{$meta:"textScore"}}).exec((err, results)=> {
+		res.send(results)
+	})
+})
+
+export default router
