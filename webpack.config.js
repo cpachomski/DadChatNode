1232e7e69b823901b9832469514d17c4e3d28727
Christopher Pachomski <cpachomski@gmail.com>
2016-09-27 13:12:32 -0400

    add webpack dev server for easy react development

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,17 @@
-import path from 'path'
+// based on NODE_ENV have global api base be localhost:3000
+//server files from localhost:8080 and change pug to pull from live reload server
+var webpack = require('webpack');
 
-export const CLIENT_DIR = __dirname + '/client'
-export const SERVER_DIR = __dirname + '/server'
-export const DIST_DIR   = __dirname + '/dist'
-
-export const babelLoader = {
-	test: /\.js$/,
-	include: CLIENT_DIR,
-	exclude: /node_modules/,
-	loader: 'babel-loader',
-}
-
-export const cssLoader = {
-	test: /\.scss$/,
-	loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
-}
-
-
-export const client = {
-	name: 'client',
-	target: 'web',
-	context: CLIENT_DIR,
-	entry: './index.js',
+module.exports  = {
+	entry: [ 
+		'webpack-dev-server/client?http://localhost:8080',
+    	'webpack/hot/only-dev-server',
+    	__dirname + '/client/index.js'
+    	],
 	output: {
-		path: DIST_DIR,
-		filename: 'bundle.js'
+		path: __dirname + '/public',
+		filename: 'bundle.js',
+		publicPath: '/static/'
 	},
-	module: {
-		loaders: [babelLoader, cssLoader]
-	},
-	resolve: {
-		extensions: ['', '.js', '.scss']
-	},
-	plugins: [
-		new ExtractTextPlugin('bundle.css', {allChunks: true})
-	]
+	plugins:[new webpack.HotModuleReplacementPlugin()]
 }
-
-export const server = {
-	name: 'server',
-	target: 'node',
-	context: CLIENT_DIR,
-	entry: {
-		app: 'components/index.js'
-	},
-	output: {
-		path: SERVER_DIR,
-		filename: '[name].js',
-		libraryTarget: 'commonjs2'
-	},
-	externals: /^[a-z\-0-9]+$/,
-	module: {
-		loaders: [babelLoader, cssLoader]
-	},
-	resolve: {
-		extensions: ['', '.js', '.scss']
-	},
-	plugins: [
-    	new ExtractTextPlugin('[name].css')
-  	]
-}
-
-export default [client, server]
