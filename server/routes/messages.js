6d0a8790d409ed2b2c04db81f02aaed13948c9a5
Christopher Pachomski <cpachomski@gmail.com>
2016-10-04 11:43:29 -0400

    create subroute to get room"s messages

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -9,9 +9,9 @@ router.get('/:id/messages', requiresLoggedIn, (req, res, next) => {
 
 	Room.findOne({ _id: roomId }, (err, room) => {
 		if (err) { next(err) }
-			
-		// return last 50 messages 	
-		let messages = room.messages.slice(1).slice(-50)
+
+		// return most recent 100 messages 	
+		let messages = room.messages.slice(1).slice(-100)
 		res.send(messages);
 	})
 })
