f36703079c5d4188d5ab1a01c6d9042b32da9378
Christopher Pachomski <cpachomski@gmail.com>
2016-10-04 11:40:10 -0400

    request messages for the room on page load and populate chat window with last 100 messages from the chat

--- client/components/ChatRoom.js
+++ client/components/ChatRoom.js
@@ -3,21 +3,36 @@ import ChatWindow from './ChatWindow'
 
 export default class ChatRoom extends Component {
 
-	componentWillMount() {
-		this.socket = io.connect(`${socketUrl}:3000`)
-		this.roomId = roomId
-		this.userId = userId
-		this.messages = messages
-		console.log(messages)
+	constructor(props) {
+		super(props)
+		this.state = {
+			socket: io.connect(`${socketUrl}:3000`),
+			roomId: roomId,
+			userId: userId,
+			messages: []
+		}
+	}
+
+	componentDidMount() {
+		this.fetchMessages(this.state.roomId)
+	}
+
+	fetchMessages(roomId) {
+		$.getJSON(`/rooms/${roomId}/messages`, (messages) => {
+			this.setState({
+				messages: messages
+			})
+		})
 	}
 
 	render() {
 		return(
 			<div id='chat-room' className='container'>
 				<div className='row'>
-					<ChatWindow socket={ this.socket }
-								roomId={ this.roomId }
-								userId={ this.userId } />
+					<ChatWindow socket={ this.state.socket }
+								roomId={ this.state.roomId }
+								userId={ this.state.userId }
+								messages={ this.state.messages } />
 				</div>
 			</div>
 		)
