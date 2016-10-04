dafd8d1d0f81c143f65ccbe5935b782657122141
Christopher Pachomski <cpachomski@gmail.com>
2016-10-04 11:40:36 -0400

    fill chat window with messages components

--- client/components/ChatWindow.js
+++ client/components/ChatWindow.js
@@ -1,9 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import uuid from 'node-uuid'
+import Message from './Message'
 
 export default class ChatWindow extends Component {
+
+	componentDidMount() {
+		console.log($)
+		$('#messages').scrollTop($('#messages')[0].scrollHeight);
+	}
+
 	render() {
+		const { messages } = this.props
+
 		return (
 			<div id='chat-window' className='medium-6 medium-offset-1 columns'>
+				<div id='messages'>
+					{ messages.map((message) => {
+						let msgKey = uuid.v4()
+
+						return (
+							<Message key={ msgKey } message={ message } />
+						)
+					})}
+				</div>
 			</div>
 		)
 	}
