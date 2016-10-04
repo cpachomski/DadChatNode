000379578a7ac7e0e981cb5737a4e79fa8fece08
Christopher Pachomski <cpachomski@gmail.com>
2016-10-04 11:41:06 -0400

    get scss includes working

--- client/index.js
+++ client/index.js
@@ -2,4 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import ChatRoom from './components/ChatRoom'
 
+import './styles/main.scss'
+
 render(<ChatRoom />, document.getElementById('dashboard'))
